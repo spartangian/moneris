c807f8a7897046befa9d96cc563b89293e5d0424
spartangian <gian.abinadab@gmail.com>
2023-04-26 13:45:27 -0500

    added handlers

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
 let url = 'http://10.0.0.224:8082/api.php';
-var myCheckout = new monerisCheckout();
+let myCheckout = new monerisCheckout();
 myCheckout.setMode("qa");
 myCheckout.setCheckoutDiv("monerisCheckout");
 
+
 let body= JSON.stringify({'total' : '300.00'});
+
+const myPageLoad = (params) =>{
+    console.log(params);
+}
 
 async function sendRequest(){
     fetch(url, {
@@ -32,4 +37,29 @@ xhr.onreadystatechange = function(){
 }
 xhr.send(body);
 
-//sendRequest();//.then(data => console.log(data));
+const myPaymentReceipt = (params) => {
+    console.log(params);
+}
+
+const myPaymentComplete = (params) => {
+    console.log(params);
+}
+
+const myErrorEvent = (params) => {
+    console.log(params);
+}
+
+const myCancelTransaction = (params) => {
+    console.log(params);
+}
+
+const myPageClosed = (params) => {
+    console.log(params);
+}
+
+myCheckout.setCallback("page_loaded", myPageLoad);
+myCheckout.setCallback("cancel_transaction", myCancelTransaction);
+myCheckout.setCallback("error_event", myErrorEvent);
+myCheckout.setCallback("payment_receipt", myPaymentReceipt);
+myCheckout.setCallback("payment_complete", myPaymentComplete);
+myCheckout.setCallback("page_closed", myPageClosed);
